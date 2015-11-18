e261ccaca00d4b0e7e1fac8414799c3c5ae4f302
gogadziba <gogadziba@gmail.com>
2015-11-18 21:43:59 +0200

    Task_4_5

---
+++ src/Task_4_5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * 4.5. Вводим 2 числа. Если первое число больше второго, то вывести на экран разницу чисел.
+ Если второе больше, то выводим сумму.
+
+ * Created by Vasssa on 18.11.2015.
+ */
+public class Task_4_5 {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int number1 = scanner1.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Enter number 2: ");
+        int number2 = scanner2.nextInt();
+        int result;
+
+        if (number1 > number2)
+            System.out.println(result = number1 - number2);
+        else {
+            System.out.println(result = number1 + number2);
+        }
+        }
+    }
+
+
