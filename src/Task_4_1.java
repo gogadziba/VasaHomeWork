6585689dce07d13c4f05049a136aea82ce044f09
gogadziba <gogadziba@gmail.com>
2015-11-18 13:59:54 +0200

    Task_4_1

---
+++ src/Task_4_1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Task_4_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter time: ");
+
+        int time = scanner.nextInt();
+
+        if(time == 9){
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 10) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 11) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 12) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 13) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 14) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 15) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 16) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 17) {
+            System.out.println("I'm still at work ");
+        }
+        else if (time == 18) {
+            System.out.println("I'm still at work ");
+        }
+        else System.out.println("I'm not at work");
+    }
+}
+
+
