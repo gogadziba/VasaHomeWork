23b37d6a66b463b6beea41134b7ad2d8223a6a4f
gogadziba <gogadziba@gmail.com>
2015-11-20 18:08:53 +0200

    Task_4_10

---
+++ src/Task_4_10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * 1) Ввести с клавиатуры значения трех сторон треугольника a, b и c и опреде-
+ * лить, является ли он прямоугольным. Ответ вывести в виде сообщения.
+ */
+public class Task_4_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number1: ");
+        double a = scanner.nextInt();
+        System.out.print("Enter number2: ");
+        double b = scanner.nextInt();
+        System.out.print("Enter number3: ");
+        double c = scanner.nextInt();
+        double d = Math.pow(a, b);
+
+        if (d == c)
+            System.out.println("Correct rectangular triangle");
+        else {
+            System.out.println("Incorrect triangle");
+        }
+    }
+}
